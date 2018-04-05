e83acf465d024b72ba4ed22576a11ac3ee5f2c89
Song Zhou <zhousong1993@gmail.com>
2018-04-05 10:23:12 +0800

    add generated sample fiels

---
+++ Sample.h
@@ -0,0 +1,20 @@
+// Sample.h
+// 
+// Created by songzhou on 2018/04/05
+// Model file Generated using objc_export https://github.com/gogozs/objc_export
+
+#import <Foundation/Foundation.h>
+
+@interface Sample : NSObject
+
+@property (nonatomic, copy) NSString* someString;
+@property (nonatomic) NSNumber* someInt;
+@property (nonatomic) NSNumber* someMinusInt;
+@property (nonatomic) NSNumber* someFloat;
+@property (nonatomic) id someObject;
+@property (nonatomic, copy) NSArray* someArray;
+@property (nonatomic) BOOL someTrue;
+@property (nonatomic) BOOL someFalse;
+@property (nonatomic) NSNull* someNull;
+
+@end
---
+++ Sample.m
@@ -0,0 +1,7 @@
+#import "Sample.h"
+
+@interface Sample ()
+@end
+
+@implementation Sample
+@end
